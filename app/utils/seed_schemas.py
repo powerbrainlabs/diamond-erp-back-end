2009d8e34712a996bfa1df2034e007cebb9a6418
shuhaib <kvshuhaib2@gmail.com>
2026-04-25 17:41:24 +0530

    stable

--- app/utils/seed_schemas.py
+++ app/utils/seed_schemas.py
@@ -9,6 +9,7 @@ async def seed_default_attributes(db):
     now = datetime.utcnow()
     system_author = {"user_id": "system", "name": "System", "email": "system"}
 
+
     # Diamond attributes
     diamond_colors = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O-Z"]
     diamond_clarity = ["FL", "IF", "VVS1", "VVS2", "VS1", "VS2", "SI1", "SI2", "I1", "I2", "I3"]
